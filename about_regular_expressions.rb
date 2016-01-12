2ddab1eb16cf8205059a859a8f19cf8b9c7d5f1b
lindani <lindani@projectcodex.co>
2016-01-12 11:50:11 +0200

    Fix character_classes_can_include_ranges test

--- about_regular_expressions.rb
+++ about_regular_expressions.rb
@@ -55,16 +55,16 @@ class AboutRegularExpressions < Neo::Koan
   end
 
   def test_slash_d_is_a_shortcut_for_a_digit_character_class
-    assert_equal "The number is 42", "the number is 42"[/[0123456789]+/]
+    assert_equal "42", "the number is 42"[/[0123456789]+/]
     assert_equal "42", "the number is 42"[/\d+/]
   end
 
   def test_character_classes_can_include_ranges
-    assert_equal __, "the number is 42"[/[0-9]+/]
+    assert_equal "42", "the number is 42"[/[0-9]+/]
   end
 
   def test_slash_s_is_a_shortcut_for_a_whitespace_character_class
-    assert_equal __, "space: \t\n"[/\s+/]
+    assert_equal "space: ", "space: \t\n"[/\s+/]
   end
 
   def test_slash_w_is_a_shortcut_for_a_word_character_class
