87a7da3180bb0eb0c811b22d2230d4622ec30a6b
lindani <lindani@projectcodex.co>
2016-01-14 15:48:18 +0200

    Add code to analyze the length of triangle sides in triangle method

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,28 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	hypotenus, side1, side2 = if c > a || c > b
+								[c, a, b]
+							elsif b > a || b >c
+								[b, a,  c]
+							else
+								[a, b, c]
+							end
+
+	if a <= 0 || b <=0 || c <= 0
+		raise TriangleError
+	elsif side1 + side2 <= hypotenus
+		raise TriangleError
+	end
+
+	type  = if hypotenus == side1 && hypotenus == side2
+			:equilateral
+		elsif hypotenus == side1 || hypotenus == side2
+			:isosceles
+		else
+			:scalene
+		end
+
 end
 
 # Error class used in part 2.  No need to change this code.
