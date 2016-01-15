0a755a9a232429c8fce5d16f32b5856f675aa116
lindani <lindani@projectcodex.co>
2016-01-15 17:42:26 +0200

    Add sandwich code for method

--- about_sandwich_code.rb
+++ about_sandwich_code.rb
@@ -104,7 +104,7 @@ class AboutSandwichCode < Neo::Koan
     end
   end
 
-  def test_open_handles_the_file_sandwich_when_given_a_block
+  def test_open_handles_the_file_sandwich_when_given_a_block  
     assert_equal 4, count_lines3("example_file.txt")
   end
 
